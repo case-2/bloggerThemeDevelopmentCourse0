8ca3734b5ee017987d028f89a2a6b535b149e025
case-2 <case000002@gmail.com>
2023-05-12 14:06:11 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   index.html
    	modified:   main.js
    	modified:   style.css
    added mobile responsiveness and mobile navigation menu

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 const searchIcon = document.querySelector('.search-icon');
 const searchContainer = document.querySelector('.search-container');
+const mobileMenuContainer = document.querySelector('.mobile-menu-container');
+const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+const menuCloseIcon = document.querySelector('.menu-close-icon');
+
 
 searchIcon.addEventListener('click', function(){
   searchContainer.classList.toggle('active');
+});
+
+mobileMenuIcon.addEventListener('click', function(){
+  mobileMenuContainer.classList.add('active');
+});
+
+menuCloseIcon.addEventListener('click', function(){
+  mobileMenuContainer.classList.remove('active');
 });
